95820ed2e94900853eda1e401dc3a299f4dfaa88
askar9-9 <askaromarbekov9.9@gmail.com>
2024-07-30 20:30:20 +0500

    feat: completed task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n *= -1
+    }
+    
+    let arr = []
+
+    while (n > 0) {
+        last = n % 10
+        arr.push(last)
+        n = (n - last) / 10
+    }
+
+    return Number(arr.join(""))
 }
